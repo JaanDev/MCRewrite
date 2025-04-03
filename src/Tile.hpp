db7de96115fe208af1133c27b2c7c7c654e0617b
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-04-04 00:11:07 +0300

    small improvements

--- src/Tile.hpp
+++ src/Tile.hpp
@@ -7,5 +7,4 @@
 
 namespace Tile {
     void renderFace(std::vector<ChunkVertex>& vertices, Level& level, int tileID, const glm::vec3& pos, Faces face);
-    void renderFaceMin(std::vector<float>& vertices, const glm::vec3& pos, Faces face);
 }
