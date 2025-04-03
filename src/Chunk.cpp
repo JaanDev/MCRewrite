b824319a5b2db7e9edbb565bc9fc8e36ebf347ab
Kolyah35 <Kolyah35@gmail.com>
2025-04-03 22:37:41 +0300

    float -> int

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -21,7 +21,7 @@ Chunk::Chunk(Level& level, const glm::ivec3& min, const glm::ivec3& max) : m_lev
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(ChunkVertex), (void*)offsetof(ChunkVertex, u));
     glEnableVertexAttribArray(2);
 
-    glVertexAttribPointer(3, 1, GL_FLOAT, GL_FALSE, sizeof(ChunkVertex), (void*)offsetof(ChunkVertex, shadow));
+    glVertexAttribPointer(3, 1, GL_INT, GL_FALSE, sizeof(ChunkVertex), (void*)offsetof(ChunkVertex, shadow));
     glEnableVertexAttribArray(3);
 
     glBindVertexArray(0);
--- src/Tile.cpp
+++ src/Tile.cpp
@@ -6,79 +6,79 @@ void Tile::renderFace(std::vector<ChunkVertex>& vertices, Level& level, int tile
     float minV = 0.0f;
     float maxV = minV + 16.0f / 256.0f;
     glm::vec3 shade = {0.6f, 1.f, 0.8f};
-    float b;
-    float shadow;
+    float brightness;
+    bool shadow;
 
     switch (face) {
         case Faces::Front:
-            b = level.getBrightness(glm::vec3(pos.x, pos.y, pos.z + 1)) * shade.z;
-            shadow = !(b == shade.z);
+            brightness = level.getBrightness(glm::vec3(pos.x, pos.y, pos.z + 1)) * shade.z;
+            shadow = !(brightness == shade.z);
 
             vertices.insert(vertices.end(), {
-                {pos.x,     pos.y,     pos.z + 1, b, b, b, minU, maxV, shadow},
-                {pos.x + 1, pos.y,     pos.z + 1, b, b, b, maxU, maxV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z + 1, b, b, b, maxU, minV, shadow},
-                {pos.x,     pos.y + 1, pos.z + 1, b, b, b, minU, minV, shadow}
+                {pos.x,     pos.y,     pos.z + 1, brightness, brightness, brightness, minU, maxV, shadow},
+                {pos.x + 1, pos.y,     pos.z + 1, brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z + 1, brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x,     pos.y + 1, pos.z + 1, brightness, brightness, brightness, minU, minV, shadow}
             });
             break;
 
         case Faces::Back:
-            b = level.getBrightness(glm::vec3(pos.x, pos.y, pos.z - 1)) * shade.z;
-            shadow = !(b == shade.z);
+            brightness = level.getBrightness(glm::vec3(pos.x, pos.y, pos.z - 1)) * shade.z;
+            shadow = !(brightness == shade.z);
 
             vertices.insert(vertices.end(), {
-                {pos.x,     pos.y,     pos.z, b, b, b, maxU, maxV, shadow},
-                {pos.x,     pos.y + 1, pos.z, b, b, b, maxU, minV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z, b, b, b, minU, minV, shadow},
-                {pos.x + 1, pos.y,     pos.z, b, b, b, minU, maxV, shadow}
+                {pos.x,     pos.y,     pos.z, brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x,     pos.y + 1, pos.z, brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z, brightness, brightness, brightness, minU, minV, shadow},
+                {pos.x + 1, pos.y,     pos.z, brightness, brightness, brightness, minU, maxV, shadow}
             });
             break;
 
         case Faces::Left:
-            b = level.getBrightness(glm::vec3(pos.x - 1, pos.y, pos.z)) * shade.x;
-            shadow = !(b == shade.x);
+            brightness = level.getBrightness(glm::vec3(pos.x - 1, pos.y, pos.z)) * shade.x;
+            shadow = !(brightness == shade.x);
 
             vertices.insert(vertices.end(), {
-                {pos.x, pos.y,     pos.z,     b, b, b, minU, maxV, shadow},
-                {pos.x, pos.y,     pos.z + 1, b, b, b, maxU, maxV, shadow},
-                {pos.x, pos.y + 1, pos.z + 1, b, b, b, maxU, minV, shadow},
-                {pos.x, pos.y + 1, pos.z,     b, b, b, minU, minV, shadow}
+                {pos.x, pos.y,     pos.z,     brightness, brightness, brightness, minU, maxV, shadow},
+                {pos.x, pos.y,     pos.z + 1, brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x, pos.y + 1, pos.z + 1, brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x, pos.y + 1, pos.z,     brightness, brightness, brightness, minU, minV, shadow}
             });
             break;
 
         case Faces::Right:
-            b = level.getBrightness(glm::vec3(pos.x + 1, pos.y, pos.z)) * shade.x;
-            shadow = !(b == shade.x);
+            brightness = level.getBrightness(glm::vec3(pos.x + 1, pos.y, pos.z)) * shade.x;
+            shadow = !(brightness == shade.x);
 
             vertices.insert(vertices.end(), {
-                {pos.x + 1, pos.y,     pos.z,     b, b, b, maxU, maxV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z,     b, b, b, maxU, minV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z + 1, b, b, b, minU, minV, shadow},
-                {pos.x + 1, pos.y,     pos.z + 1, b, b, b, minU, maxV, shadow}
+                {pos.x + 1, pos.y,     pos.z,     brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z,     brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z + 1, brightness, brightness, brightness, minU, minV, shadow},
+                {pos.x + 1, pos.y,     pos.z + 1, brightness, brightness, brightness, minU, maxV, shadow}
             });
             break;
 
         case Faces::Up:
-            b = level.getBrightness(glm::vec3(pos.x, pos.y + 1, pos.z)) * shade.y;
-            shadow = !(b == shade.y);
+            brightness = level.getBrightness(glm::vec3(pos.x, pos.y + 1, pos.z)) * shade.y;
+            shadow = !(brightness == shade.y);
 
             vertices.insert(vertices.end(), {
-                {pos.x,     pos.y + 1, pos.z + 1, b, b, b, minU, maxV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z + 1, b, b, b, maxU, maxV, shadow},
-                {pos.x + 1, pos.y + 1, pos.z,     b, b, b, maxU, minV, shadow},
-                {pos.x,     pos.y + 1, pos.z,     b, b, b, minU, minV, shadow}
+                {pos.x,     pos.y + 1, pos.z + 1, brightness, brightness, brightness, minU, maxV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z + 1, brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x + 1, pos.y + 1, pos.z,     brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x,     pos.y + 1, pos.z,     brightness, brightness, brightness, minU, minV, shadow}
             });
             break;
 
         case Faces::Down:
-            b = level.getBrightness(glm::vec3(pos.x, pos.y - 1, pos.z)) * shade.y;
-            shadow = !(b == shade.y);
+            brightness = level.getBrightness(glm::vec3(pos.x, pos.y - 1, pos.z)) * shade.y;
+            shadow = !(brightness == shade.y);
 
             vertices.insert(vertices.end(), {
-                {pos.x,     pos.y, pos.z,     b, b, b, minU, minV, shadow},
-                {pos.x + 1, pos.y, pos.z,     b, b, b, maxU, minV, shadow},
-                {pos.x + 1, pos.y, pos.z + 1, b, b, b, maxU, maxV, shadow},
-                {pos.x,     pos.y, pos.z + 1, b, b, b, minU, maxV, shadow}
+                {pos.x,     pos.y, pos.z,     brightness, brightness, brightness, minU, minV, shadow},
+                {pos.x + 1, pos.y, pos.z,     brightness, brightness, brightness, maxU, minV, shadow},
+                {pos.x + 1, pos.y, pos.z + 1, brightness, brightness, brightness, maxU, maxV, shadow},
+                {pos.x,     pos.y, pos.z + 1, brightness, brightness, brightness, minU, maxV, shadow}
             });
             break;
     }
